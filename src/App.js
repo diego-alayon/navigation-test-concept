d210d7bd2884e02cbcb377013e3069ac94c35552
Diego Alayon Gomez <30353719+diego-alayon@users.noreply.github.com>
2021-11-15 17:55:23 +0100

    items-navigation file to navigation component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { ProSidebar, SidebarFooter, SidebarContent, SubMenu, MenuItem, Menu } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
 import React, { useState } from 'react'
-import DataNavigation from './bbdd/data-navigation.json'
+// import DataNavigation from './bbdd/data-navigation.json'
+import { Sites, Boards, Scaning, Inventory, Software, Reports, Configuration } from './bbdd/items-navigation'
 import {
   UilAngleDoubleLeft,
   UilBrowser,
@@ -27,94 +28,49 @@ const App = ({collapsed}) => {
   return (
     <>
       <ProSidebar collapsed={toggle ? true : false} className="pro-sidebar-custom">
-      <div>
-        { DataNavigation.map(post => {
-          return(
-            <>
-              <div key={post.subitems}>
-                <div>{ post.subitems }</div>
-                {console.log(post.subitems)}
-              </div>
-            </>
-          )
-        })}
-      </div>
         <SidebarContent>
         <Menu iconShape="square">
         <MenuItem icon={<UilAngleDoubleLeft onClick={() => setToggle(!toggle)} />}>Lansweeper</MenuItem>
         <SubMenu icon={<UilBrowser />} title="Sites">
-          <MenuItem>Doublegapac-1</MenuItem>
-          <MenuItem>Doublegapac-1</MenuItem>
-          <MenuItem>Doublegapac-1</MenuItem>
-          <MenuItem>Doublegapac-1</MenuItem>
-          <MenuItem>Doublegapac-1</MenuItem>
-          <MenuItem>Doublegapac-1</MenuItem>
-          <MenuItem>Doublegapac-1</MenuItem>
-          <MenuItem>Doublegapac-1</MenuItem>
+          <MenuItem>
+            {Sites.map(Sites => <MenuItem>{Sites}</MenuItem>)}
+          </MenuItem>
         </SubMenu>
         
         <SubMenu icon={<UilDashboard />} title="Boards">
-          <MenuItem>Overview</MenuItem>
           <MenuItem>
-            <div className="target-link">
-              <div className="mr-2">Create new board</div>
-              <UilPlusCircle />
-            </div>
+            {Boards.map(Boards => <MenuItem>{Boards}</MenuItem>)}
           </MenuItem>
-          <SubMenu title="Components">
-            <MenuItem>Component 2</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-          </SubMenu>
         </SubMenu>
 
         <SubMenu icon={<UilFocusTarget />} title="Scaning">
-          <MenuItem>All scan servers</MenuItem>
-          <MenuItem>targets</MenuItem>
-          <MenuItem>Credential vault</MenuItem>
-          <MenuItem>Mapping</MenuItem>
-          <MenuItem>Asset radar</MenuItem>
-          <MenuItem>Exclusions</MenuItem>
-          <MenuItem>Scan issues</MenuItem>
+          <MenuItem>
+            {Scaning.map(Scaning => <MenuItem>{Scaning}</MenuItem>)}
+          </MenuItem>
         </SubMenu>
 
         <SubMenu icon={<UilDesktopAlt />} title="Inventory">
-          <MenuItem>All assets</MenuItem>
-          <MenuItem>Assets groups</MenuItem>
-          <MenuItem>Assets States</MenuItem>
-          <MenuItem>Assets types</MenuItem>
-          <MenuItem>Domains & Workgroups</MenuItem>
-          <MenuItem>Installations</MenuItem>
-          <MenuItem>IP Locations</MenuItem>
-          <MenuItem>Asset Radar</MenuItem>
-          <MenuItem>Duplicate conflicts</MenuItem>
-          <MenuItem>Add new asset</MenuItem>
+          <MenuItem>
+            {Inventory.map(Inventory => <MenuItem>{Inventory}</MenuItem>)}
+          </MenuItem>
         </SubMenu>
 
         <SubMenu icon={<UilCreditCardSearch />} title="Software">
-          <MenuItem>Software</MenuItem>
+          <MenuItem>
+            {Software.map(Software => <MenuItem>{Software}</MenuItem>)}
+          </MenuItem>
         </SubMenu>
 
         <SubMenu icon={<UilChart />} title="Reports">
-          <MenuItem>All reports</MenuItem>
-          <MenuItem>Hardware</MenuItem>
-          <MenuItem>Security</MenuItem>
-          <MenuItem>Software</MenuItem>
-          <MenuItem>Users</MenuItem>
-          <MenuItem>Custom reports</MenuItem>
-          <MenuItem>Create new report</MenuItem>
+          <MenuItem>
+            {Reports.map(Reports => <MenuItem>{Reports}</MenuItem>)}
+          </MenuItem>
         </SubMenu>
 
         <SubMenu icon={<UilCog />} title="Configuration">
-          <MenuItem>Branding</MenuItem>
-          <MenuItem>Installations</MenuItem>
-          <MenuItem>Account management</MenuItem>
-          <MenuItem>Asset management</MenuItem>
-          <MenuItem>Aplications</MenuItem>
-          <MenuItem>Server settings</MenuItem>
+          <MenuItem>
+            {Configuration.map(Configuration => <MenuItem>{Configuration}</MenuItem>)}
+          </MenuItem>
         </SubMenu>
 
       </Menu>
---
+++ src/bbdd/items-navigation.js
@@ -0,0 +1,60 @@
+export const Sites = [
+    'Site-1',
+    'Site-2',
+    'Site-3',
+    'Site-4',
+    'Site-5',
+    'Site-6',
+    'Site-7',
+    'Site-8',
+    'Site-9',
+    'Site-10',
+]
+export const Boards = [
+    'Overview',
+    'Create new board',
+    'Components'
+]
+export const Scaning = [
+    'All scan servers',
+    'targets',
+    'Credential vault',
+    'Mapping',
+    'Asset radar',
+    'Exclusions',
+    'Scan issues'
+]
+export const Inventory = [
+    'All assets',
+    'Assets groups',
+    'Assets States',
+    'Assets types',
+    'Domains & Workgroups',
+    'Installations',
+    'IP Locations',
+    'Asset Radar',
+    'Duplicate conflicts',
+    'Add new asset'
+]
+export const Software = [
+    'Software'
+]
+export const Reports = [
+    'All reports',
+    'Hardware',
+    'Security',
+    'Software',
+    'Users',
+    'Custom reports',
+    'Create new report'
+]
+export const Configuration = [
+    'Branding',
+    'Installations',
+    'Account management',
+    'Asset management',
+    'Aplications',
+    'Server settings'
+]
+export const WhatsNew = []
+export const Notifications = []
