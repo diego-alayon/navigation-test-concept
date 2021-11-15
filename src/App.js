c84028830e718174445fe8ea9d284a2ec103e78e
Diego Alayon Gomez <30353719+diego-alayon@users.noreply.github.com>
2021-11-15 13:28:17 +0100

    setup navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { ProSidebar, SidebarFooter, SidebarContent, SubMenu, MenuItem, Menu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import React, { useState } from 'react'
+import DataNavigation from './bbdd/data-navigation.json'
+import {
+  UilAngleDoubleLeft,
+  UilBrowser,
+  UilDashboard,
+  UilDesktopAlt,
+  UilFocusTarget,
+  UilCreditCardSearch,
+  UilChart,
+  UilCog,
+  UilMegaphone,
+  UilNewspaper,
+  UilPlusCircle
+ } from '@iconscout/react-unicons';
+ import './custom.scss'
+
+
+const App = ({collapsed}) => {
+
+  const [toggle, setToggle] = useState(collapsed)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ProSidebar collapsed={toggle ? true : false} className="pro-sidebar-custom">
+      <div>
+        { DataNavigation.map(post => {
+          return(
+            <>
+              <div key={post.subitems}>
+                <div>{ post.subitems }</div>
+                {console.log(post.subitems)}
+              </div>
+            </>
+          )
+        })}
+      </div>
+        <SidebarContent>
+        <Menu iconShape="square">
+        <MenuItem icon={<UilAngleDoubleLeft onClick={() => setToggle(!toggle)} />}>Lansweeper</MenuItem>
+        <SubMenu icon={<UilBrowser />} title="Sites">
+          <MenuItem>Doublegapac-1</MenuItem>
+          <MenuItem>Doublegapac-1</MenuItem>
+          <MenuItem>Doublegapac-1</MenuItem>
+          <MenuItem>Doublegapac-1</MenuItem>
+          <MenuItem>Doublegapac-1</MenuItem>
+          <MenuItem>Doublegapac-1</MenuItem>
+          <MenuItem>Doublegapac-1</MenuItem>
+          <MenuItem>Doublegapac-1</MenuItem>
+        </SubMenu>
+        
+        <SubMenu icon={<UilDashboard />} title="Boards">
+          <MenuItem>Overview</MenuItem>
+          <MenuItem>
+            <div className="target-link">
+              <div className="mr-2">Create new board</div>
+              <UilPlusCircle />
+            </div>
+          </MenuItem>
+          <SubMenu title="Components">
+            <MenuItem>Component 2</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+          </SubMenu>
+        </SubMenu>
+
+        <SubMenu icon={<UilFocusTarget />} title="Scaning">
+          <MenuItem>All scan servers</MenuItem>
+          <MenuItem>targets</MenuItem>
+          <MenuItem>Credential vault</MenuItem>
+          <MenuItem>Mapping</MenuItem>
+          <MenuItem>Asset radar</MenuItem>
+          <MenuItem>Exclusions</MenuItem>
+          <MenuItem>Scan issues</MenuItem>
+        </SubMenu>
+
+        <SubMenu icon={<UilDesktopAlt />} title="Inventory">
+          <MenuItem>All assets</MenuItem>
+          <MenuItem>Assets groups</MenuItem>
+          <MenuItem>Assets States</MenuItem>
+          <MenuItem>Assets types</MenuItem>
+          <MenuItem>Domains & Workgroups</MenuItem>
+          <MenuItem>Installations</MenuItem>
+          <MenuItem>IP Locations</MenuItem>
+          <MenuItem>Asset Radar</MenuItem>
+          <MenuItem>Duplicate conflicts</MenuItem>
+          <MenuItem>Add new asset</MenuItem>
+        </SubMenu>
+
+        <SubMenu icon={<UilCreditCardSearch />} title="Software">
+          <MenuItem>Software</MenuItem>
+        </SubMenu>
+
+        <SubMenu icon={<UilChart />} title="Reports">
+          <MenuItem>All reports</MenuItem>
+          <MenuItem>Hardware</MenuItem>
+          <MenuItem>Security</MenuItem>
+          <MenuItem>Software</MenuItem>
+          <MenuItem>Users</MenuItem>
+          <MenuItem>Custom reports</MenuItem>
+          <MenuItem>Create new report</MenuItem>
+        </SubMenu>
+
+        <SubMenu icon={<UilCog />} title="Configuration">
+          <MenuItem>Branding</MenuItem>
+          <MenuItem>Installations</MenuItem>
+          <MenuItem>Account management</MenuItem>
+          <MenuItem>Asset management</MenuItem>
+          <MenuItem>Aplications</MenuItem>
+          <MenuItem>Server settings</MenuItem>
+        </SubMenu>
+
+      </Menu>
+        </SidebarContent>
+
+        <SidebarFooter>
+          <Menu>
+            <MenuItem icon={<UilMegaphone />}>What's new</MenuItem>
+            <MenuItem icon={<UilNewspaper />}>Notifications</MenuItem>
+          </Menu>
+        </SidebarFooter>
+      </ProSidebar>
+    </>
   );
 }
 
